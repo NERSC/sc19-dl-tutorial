5e84b94cf6c6685c02172025735fce60094844af
Mustafa Mustafa <mstftsm@gmail.com>
2019-05-31 17:22:55 -0700

    Update for ISC19

--- scripts/cifar_resnet.sh
+++ scripts/cifar_resnet.sh
@@ -2,9 +2,8 @@
 #SBATCH -J cifar-resnet
 #SBATCH -C knl
 #SBATCH -N 1
-#SBATCH -q regular
-#SBATCH --reservation=cug_analytics_2019
-#SBATCH -t 45
+#SBATCH -q debug
+#SBATCH -t 30
 #SBATCH -o logs/%x-%j.out
 
 module load tensorflow/intel-1.13.1-py36
