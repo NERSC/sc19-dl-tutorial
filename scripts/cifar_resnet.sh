bc6dbfb595208f10dce6ecfc1fde5869ec068d24
Steve Farrell <sparticlesteve@gmail.com>
2019-11-13 09:29:39 -0800

    update batch scripts for testing

--- scripts/cifar_resnet.sh
+++ scripts/cifar_resnet.sh
@@ -2,14 +2,13 @@
 #SBATCH -J cifar-resnet
 #SBATCH -C knl
 #SBATCH -N 1
-#SBATCH -q regular
-#SBATCH --reservation isc19_dl_tutorial
+#SBATCH -q debug
 #SBATCH -t 30
 #SBATCH -o logs/%x-%j.out
 
+##SBATCH --reservation dl4sci_sc19
+
 module load tensorflow/intel-1.13.1-py36
-
 config=configs/cifar10_resnet.yaml
-script=train.py
 
-srun python $script $config -d
+srun python train.py $config -d
