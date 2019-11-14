168c7c0d1b20b01d79fc110544310431741e0dc8
Ben Albrecht <balbrecht@cray.com>
2019-11-14 14:15:42 -0800

    Revert scripts and configs to master

--- scripts/cifar_cnn.sh
+++ scripts/cifar_cnn.sh
@@ -3,12 +3,19 @@
 #SBATCH -C knl
 #SBATCH -N 1
 #SBATCH -q debug
-#SBATCH -t 30:00
+#SBATCH -t 30
 #SBATCH -o logs/%x-%j.out
 
+##SBATCH --reservation dl4sci_sc19
+
+# Load the software
 module load tensorflow/intel-1.13.1-py36
-
+export KMP_BLOCKTIME=0
+export KMP_AFFINITY="granularity=fine,compact,1,0"
 config=configs/cifar10_cnn.yaml
-script=train.py
 
-srun python $script $config -d
+# Ensure dataset is downloaded by single process
+python -c "import keras; keras.datasets.cifar10.load_data()"
+
+# Run the training
+srun python train.py $config -d
--- scripts/cifar_resnet.sh
+++ scripts/cifar_resnet.sh
@@ -6,10 +6,16 @@
 #SBATCH -t 30
 #SBATCH -o logs/%x-%j.out
 
-# BA: 1024 KNL nodes (~4 nodes/person?)
+##SBATCH --reservation dl4sci_sc19
+
+# Load the software
 module load tensorflow/intel-1.13.1-py36
-
+export KMP_BLOCKTIME=0
+export KMP_AFFINITY="granularity=fine,compact,1,0"
 config=configs/cifar10_resnet.yaml
-script=train_horovod.py
 
-srun python $script $config -d
+# Ensure dataset is downloaded by single process
+python -c "import keras; keras.datasets.cifar10.load_data()"
+
+# Run the training
+srun python train.py $config -d
